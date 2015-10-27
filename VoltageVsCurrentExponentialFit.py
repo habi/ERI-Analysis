05aef19334091e024bfb364fa61b4d7bfd45527d
David Haberthür <email@davidhaberthuer.ch>
2015-10-27 17:07:57 +0100

    Nicer display and better fitting
    The user is also warned if the fitting library cannot be loaded, the
    script can only be run in the terminal after the Enthought Python
    Distribution has been loaded.

--- VoltageVsCurrentExponentialFit.py
+++ VoltageVsCurrentExponentialFit.py
@@ -10,26 +10,19 @@ import os
 import glob
 import matplotlib.pylab as plt
 import numpy
-try:
-    # import scipy.optimize
-    import scipy.stats
-except ImportError:
-    print 'To use this script, we need to load the Enthought Python ' \
-          'distribution. To do so, please enter the command below into the ' \
-          'terminal and restart the script.\n'
-    print 'module load xbl/epd\n'
-    exit()
+import scipy.optimize
+import scipy.stats
 
 StartPath = os.path.join(os.path.expanduser('~'), 'Data20', 'Gantry', 'Images')
 ERIFolders = glob.glob(os.path.join(StartPath, 'ERI*'))
 
 # Colors from 'I want hue!'
-colors = ["#82DFB2",
-          "#DAB5D6",
-          "#E9AD71",
-          "#8DD6DF",
-          "#D8D56D",
-          "#A4E280"]
+colors = ["#84DEBD",
+          "#D1B9D4",
+          "#D1D171"]
+# Wider default curves, better markers and grid
+plt.rc('lines', linewidth=2, marker='o')
+plt.rc('axes', grid=True)
 
 
 def fitting_function(x, a, b, c):
@@ -46,27 +39,35 @@ for counter, folder in enumerate(ERIFolders):
                            image in ImageList])
     Current = numpy.array([int(os.path.basename(image).split('_')[2][:-2]) for
                            image in ImageList])
-    plt.scatter(Voltage, Current, label=os.path.basename(folder),
-                c=colors[counter+len(ERIFolders)])
-    InitialGuess = (0.07, 0.095, 9.5)
-    OptimalValues, Covariance = scipy.optimize.curve_fit(fitting_function,
-                                                         Voltage, Current,
-                                                         p0=InitialGuess)
-    print '\tThe best exponential fit to the data is %s * numpy.exp(%s * x) ' \
-          '+ %s' % (OptimalValues[0], OptimalValues[1], OptimalValues[2])
-    plt.plot(Voltage, fitting_function(Voltage, *OptimalValues), 'r-',
+    plt.scatter(Voltage, Current, label=os.path.basename(folder), c=colors[0])
+    InitialGuess = (0.056, 9.9e-2, 9.425)
+    try:
+        OptimalValues, Covariance = scipy.optimize.curve_fit(fitting_function,
+                                                             Voltage, Current,
+                                                             p0=InitialGuess)
+    except AttributeError:
+        exit('You need to "module load xbl/epd" to make this script work\nAs '
+             'a consequence, the script cannot be run in PyCharm, but has to '
+             'be run in the terminal...')
+    print '\tThe best exponential fit to the data is %0.2e * numpy.exp(%0.2e ' \
+          '* x) + %0.2e' % (OptimalValues[0], OptimalValues[1],
+                            OptimalValues[2])
+    plt.plot(Voltage, fitting_function(Voltage, *OptimalValues), '-',
+             c=colors[1],
              label='Fitted Curve (%0.2f*e^%0.2e*x+%0.2f)' % (OptimalValues[0],
                                                              OptimalValues[1],
                                                              OptimalValues[2]))
     ManualFit = [InitialGuess[0] * numpy.exp(InitialGuess[1] * i) +
                  InitialGuess[2] for i in Voltage]
-    plt.plot(Voltage, ManualFit, 'g-',
+    plt.plot(Voltage, ManualFit, '-', c=colors[2],
              label='Empirical fit (%0.2f*e^%0.2e*x+%0.2f)' % (InitialGuess[0],
                                                               InitialGuess[1],
                                                               InitialGuess[2]))
     plt.xlabel('Voltage [kV]')
     plt.ylabel('Current [uA]')
-    plt.xlim([0, 66])
+    plt.xlim([20, 70])
     plt.ylim(ymin=0)
     plt.legend(loc='best')
+plt.savefig(os.path.join(os.path.expanduser('~'), 'Data20', 'CNT',
+                         'ERI-Analysis', 'Images', 'ExponentialFit.png'))
 plt.show()
