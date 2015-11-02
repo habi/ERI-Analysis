ae20127233bf2272dd8033d934841bdb8b1bed8b
David Haberthür <email@davidhaberthuer.ch>
2015-11-02 17:25:42 +0100

    Plot brightness differences
    With the same settings, the Hamamatsu source seems to be more powerful
    than the ERI source, as can be seen in the resulting plots.

--- BrightnessComparison.py
+++ BrightnessComparison.py
@@ -51,7 +51,7 @@ def contrast_stretch(image, std=3, verbose=False):
 # Display all images consistently
 plt.rc('image', cmap='gray', interpolation='nearest')
 # Make lines a bit wider
-plt.rc('lines', linewidth=2)
+plt.rc('lines', linewidth=2, marker='o')
 # Show background grid
 plt.rc('axes', grid=True)
 # Colors from 'I want hue'
@@ -59,19 +59,32 @@ colors = ["#84DEBD", "#D1B9D4", "#D1D171"]
 
 StartPath = '/sls/X02DA/data/e13960/Data20/Gantry/Images'
 
+Case = 1  # 0 or 1
 # Grab Voltage and Current for ERI
-ImageListERI = sorted(glob.glob(os.path.join(StartPath,
-                                             'ERI-Voltage-Current-Curve-Random',
-                                             '*.raw')))
+if Case:
+    ImageListERI = sorted(glob.glob(os.path.join(StartPath,
+                                                 'ERI-Voltage-Current-Curve-1',
+                                                 '*.raw')))
+else:
+    ImageListERI = sorted(glob.glob(os.path.join(StartPath,
+                                                 'ERI-Voltage-Current-Curve-'
+                                                 'Random', '*.raw')))
 
 print 'Reading Voltage and Current from %s images' % len(ImageListERI)
 VoltageERI = [int(os.path.basename(i).split('_')[1][:-2]) for i in ImageListERI]
 CurrentERI = [int(os.path.basename(i).split('_')[2][:-2]) for i in ImageListERI]
 
 # Grab Voltage and Current for ERI
-ImageListHamamatsu = sorted(glob.glob(os.path.join(StartPath,
-                                                   'Hamamatsu-Flats-30s-'
-                                                   'Exposure', '*.raw')))
+if Case:
+    ImageListHamamatsu = sorted(glob.glob(os.path.join(StartPath,
+                                                       'Hamamatsu-KinderEgg-'
+                                                       '15s-Exposure',
+                                                       '*.raw')))
+else:
+    ImageListHamamatsu = sorted(glob.glob(os.path.join(StartPath,
+                                                       'Hamamatsu-KinderEgg-'
+                                                       '30s-Exposure',
+                                                       '*.raw')))
 
 print 'Reading Voltage and Current from %s images' % len(ImageListERI)
 VoltageHamamatsu = [int(os.path.basename(i).split('_')[1][:-2]) for i in
@@ -79,7 +92,9 @@ VoltageHamamatsu = [int(os.path.basename(i).split('_')[1][:-2]) for i in
 CurrentHamamatsu = [int(os.path.basename(i).split('_')[2][:-2]) for i in
                     ImageListHamamatsu]
 
-# Grab closest values from the Hamamatsu dataset
+# Grab closest values from the Hamamatsu dataset and plot them afterwards
+plt.ion()
+plt.figure(figsize=[16, 9])
 CompareImages = []
 for c, i in enumerate(ImageListERI):
     print 80 * '-'
@@ -90,12 +105,14 @@ for c, i in enumerate(ImageListERI):
         if str(VoltageERI[c]) + 'kV' in k:
             Candidates.append(k)
     IndexList = [ImageListHamamatsu.index(i) for i in Candidates]
-    # Get the closest value to the current from Hamamastu list. Based on
+    # Get the closest value to the current from Hamamatsu list. Based on
     # http://stackoverflow.com/a/9706105/323100
     ChosenOne = min(enumerate([CurrentHamamatsu[i] for i in IndexList]),
                     key=lambda x: abs(x[1]-CurrentERI[c]))
     print 'Found a match in %s' % Candidates[ChosenOne[0]]
     CompareImages.append(Candidates[ChosenOne[0]])
+
+print 80 * '-'
 
 VoltageMatch = [int(os.path.basename(i).split('_')[1][:-2]) for i in
                 CompareImages]
@@ -104,7 +121,67 @@ CurrentMatch = [int(os.path.basename(i).split('_')[2][:-2]) for i in
 
 plt.scatter(VoltageHamamatsu, CurrentHamamatsu, c=colors[0], alpha=0.25,
             label='Hamamatsu')
-plt.scatter(VoltageERI, CurrentERI, c=colors[1], label='ERI')
-plt.scatter(VoltageMatch, CurrentMatch, c=colors[2], label='Best Match')
+plt.plot(VoltageERI, CurrentERI, c=colors[1], label='ERI')
+plt.plot(VoltageMatch, CurrentMatch, c=colors[2], label='Best Match')
 plt.legend(loc='best')
+plt.draw()
+plt.savefig(os.path.join(os.path.expanduser('~'), 'Data20', 'CNT',
+                         'ERI-Analysis', 'Images',
+                         'Match-' +
+                         os.path.basename(os.path.dirname(CompareImages[0])) +
+                         '_vs_' +
+                         os.path.basename(os.path.dirname(ImageListERI[0])) +
+                         '.png'))
+
+# Plot brightness of the comparable images in one plot
+BrightnessHamamatsu = []
+BrightnessERI = []
+DisplayProgress = True
+plt.figure(figsize=[16, 9])
+for c, i in enumerate(CompareImages):
+    print '%s/%s: Comparing %s with %s' % (c + 1, len(CompareImages),
+                                           os.path.basename(i),
+                                           os.path.basename(ImageListERI[c]))
+    ImageERI = read_raw(ImageListERI[c])
+    ImageHamamatsu = read_raw(i)
+    BrightnessERI.append(numpy.mean(ImageERI))
+    BrightnessHamamatsu.append(numpy.mean(ImageHamamatsu))
+    # plt.subplot(221)
+    # plt.imshow(read_raw(ImageListERI[c]))
+    # plt.title('%s: %s' % (c, os.path.basename(ImageListERI[c])))
+    # plt.subplot(222)
+    # plt.imshow(read_raw(i))
+    # plt.title('%s: %s' % (c, os.path.basename(i)))
+    # plt.subplot(212)
+    # plt.cla()
+# Plot Brigtness with kV as x-axis (Image 0 is 25 kV)
+plt.plot(range(25, 25 + len(BrightnessHamamatsu)), BrightnessHamamatsu,
+         c=colors[2], label=os.path.basename(os.path.dirname(i)))
+plt.plot(range(25, 25 + len(BrightnessERI)), BrightnessERI, c=colors[1],
+         label=os.path.basename(os.path.dirname(ImageListERI[0])))
+plt.xlabel('kV')
+plt.ylabel('Mean image brightness')
+
+ScalingHamamatsu = numpy.max(BrightnessHamamatsu) / numpy.max(CurrentMatch)
+plt.plot(VoltageMatch, [i * ScalingHamamatsu for i in CurrentMatch],
+         c=colors[2], alpha=0.5, linestyle='--',
+         label='Hamamatsu, current scaled by %0.1f' % ScalingHamamatsu)
+ScalingERI = max(BrightnessERI) / float(max(CurrentERI))
+plt.plot(VoltageERI, [i * ScalingERI for i in CurrentERI], c=colors[1],
+         alpha=0.5, linestyle='--',
+         label='ERI: current scaled by %0.1f' % ScalingERI)
+
+plt.xlim([20, 70])
+plt.draw()
+plt.legend(loc='best')
+plt.savefig(os.path.join(os.path.expanduser('~'), 'Data20', 'CNT',
+                         'ERI-Analysis', 'Images',
+                         'Brightness-' +
+                         os.path.basename(os.path.dirname(CompareImages[0])) +
+                         '_vs_' +
+                         os.path.basename(os.path.dirname(ImageListERI[0])) +
+                         '.png'))
 plt.show()
+
+print 'Done!'
+
