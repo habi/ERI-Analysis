bc309d7c26917fe7acf7c70042b4f1921fd1a965
David Haberthür <email@davidhaberthuer.ch>
2016-01-05 16:54:40 +0100

    Load file for ERI, display corresponding Hamamatsu
    We load the result image from the resolution script from ERI, look for
    the corresponding one from Hamamatsu and display both along side a plot
    of the details.

--- CompareResolutionPlots.py
+++ CompareResolutionPlots.py
@@ -7,10 +7,21 @@ Script to compare two line profile plots with eatch other.
 """
 
 # Imports
-import numpy
 import os
 import glob
+import platform
+import matplotlib
+# Make sure we are running a good version of matplotlib (i.e. > 1)
+if float(str(matplotlib.__version__)[:3]) < 1:
+    print '\nWe are running matplotlib version', matplotlib.__version__, 'on', \
+        platform.node()
+    print 'To make this script work, we need a matplotlib version > 1.'
+    print 'To load such a version, please enter the following command in', \
+        'the terminal and restart the script.'
+    print '\n\tmodule load xbl/epd_free/7.3-2-2013.06\n'
+    exit()
 import matplotlib.pylab as plt
+import matplotlib.gridspec as gridspec
 
 
 def AskUser(Blurb, Choices):
@@ -36,6 +47,13 @@ def AskUser(Blurb, Choices):
     return sorted(Choices)[Selection]
 
 
+def bold(msg):
+    """
+    Enable bold and colorful output on the command line (http://is.gd/HCaDv9)
+    """
+    return u'\033[1m%s\033[0m' % msg
+
+
 # Display all images consistently
 plt.rc('image', cmap='gray', interpolation='nearest')
 # Make lines a bit wider
@@ -47,8 +65,10 @@ colors = ["#84DEBD", "#D1B9D4", "#D1D171"]
 
 StartPath = '/sls/X02DA/data/e13960/Data20/CNT/ERI-Analysis/Images/'
 # Filter list for only 'Grid' folders: http://stackoverflow.com/a/4260304
-FolderList = [i if 'Grid' in i else '' for i in sorted(os.walk(StartPath).next()[1])]
-
+FolderList = [i if 'Grid' in i else '' for i in
+              sorted(os.walk(StartPath).next()[1])]
+# Filter list to remove OutputFolders
+FolderList = [i if 'Comparison' not in i else '' for i in FolderList]
 ERIFolders = [i if 'ERI' in i else '' for i in FolderList]
 HamamatsuFolders = [i if 'Hamamatsu' in i else '' for i in FolderList]
 # Disregard now emtpy list elements: http://stackoverflow.com/a/3845449
@@ -57,148 +77,92 @@ HamamatsuFolders = [x for x in HamamatsuFolders if x]
 
 ChosenERI = AskUser('Which ERI folder shall we use?', ERIFolders)
 ChosenHamamatsu = AskUser('Which Hamamatsu folder shall we use to compare '
-                          'with "%s"?' % ChosenERI, HamamatsuFolders)
+                          'with %s?' % bold(ChosenERI), HamamatsuFolders)
 
 
+# Prepare output directory
+OutputPath = os.path.join(os.path.expanduser('~'), 'Data20', 'CNT',
+                          'ERI-Analysis', 'Images', 'Comparison-' +
+                          os.path.basename(ChosenERI) + '_VS_' +
+                          os.path.basename(ChosenHamamatsu))
+try:
+    os.makedirs(OutputPath)
+except OSError:
+    # Directory already exists
+    pass
 
+ImageListERI = sorted(glob.glob(os.path.join(StartPath, ChosenERI, '*.png')))
+print 'Reading Parameters from %s images in %s' % (len(ImageListERI),
+                                                   bold(ChosenERI))
+VoltageERI = [int(os.path.basename(i).split('_')[1][:-2]) for i in ImageListERI]
+CurrentERI = [int(os.path.basename(i).split('_')[2][:-2]) for i in ImageListERI]
 
-exit()
-for Case in (0, 1):
-    # Grab Voltage and Current for ERI
-    if Case:
-        ImageListERI = sorted(glob.glob(os.path.join(StartPath,
-                                                     'ERI-Voltage-Current-Curve-1',
-                                                     '*.raw')))
-    else:
-        ImageListERI = sorted(glob.glob(os.path.join(StartPath,
-                                                     'ERI-Voltage-Current-Curve-'
-                                                     'Random', '*.raw')))
+ImageListHamamatsu = sorted(glob.glob(os.path.join(StartPath, ChosenHamamatsu,
+                                                   '*.png')))
+print 'Reading Parameters from %s images in %s' % (len(ImageListERI),
+                                                   bold(ChosenHamamatsu))
+VoltageHamamatsu = [int(os.path.basename(i).split('_')[1][:-2]) for i in
+                    ImageListHamamatsu]
+CurrentHamamatsu = [int(os.path.basename(i).split('_')[2][:-2]) for i in
+                    ImageListHamamatsu]
 
-    print 'Reading Voltage and Current from %s images' % len(ImageListERI)
-    VoltageERI = [int(os.path.basename(i).split('_')[1][:-2]) for i in ImageListERI]
-    CurrentERI = [int(os.path.basename(i).split('_')[2][:-2]) for i in ImageListERI]
+# Grab closest values from the Hamamatsu data set and plot them afterwards
+CompareImages = []
+for c, i in enumerate(ImageListERI):
+    # print 80 * '-'
+    # print 'Finding best matching current from Hamamatsu for %s (%s kV, ' \
+    #       '%s mA)' % (i, VoltageERI[c], CurrentERI[c])
+    Candidates = []
+    for k in ImageListHamamatsu:
+        if str(VoltageERI[c]) + 'kV' in k:
+            Candidates.append(k)
+    IndexList = [ImageListHamamatsu.index(i) for i in Candidates]
+    # Get the closest value to the current from Hamamatsu list. Based on
+    # http://stackoverflow.com/a/9706105/323100
+    ChosenOne = min(enumerate([CurrentHamamatsu[i] for i in IndexList]),
+                    key=lambda x: abs(x[1] - CurrentERI[c]))
+    # print 'Found a match in %s' % Candidates[ChosenOne[0]]
+    CompareImages.append(Candidates[ChosenOne[0]])
+# print 80 * '-'
+VoltageMatch = [int(os.path.basename(i).split('_')[1][:-2]) for i in
+                CompareImages]
+CurrentMatch = [int(os.path.basename(i).split('_')[2][:-2]) for i in
+                CompareImages]
 
-    # Grab Voltage and Current for ERI
-    if Case:
-        ImageListHamamatsu = sorted(glob.glob(os.path.join(StartPath,
-                                                           'Hamamatsu-KinderEgg-'
-                                                           '15s-Exposure',
-                                                           '*.raw')))
-    else:
-        ImageListHamamatsu = sorted(glob.glob(os.path.join(StartPath,
-                                                           'Hamamatsu-KinderEgg-'
-                                                           '30s-Exposure',
-                                                           '*.raw')))
-
-    print 'Reading Voltage and Current from %s images' % len(ImageListERI)
-    VoltageHamamatsu = [int(os.path.basename(i).split('_')[1][:-2]) for i in
-                        ImageListHamamatsu]
-    CurrentHamamatsu = [int(os.path.basename(i).split('_')[2][:-2]) for i in
-                        ImageListHamamatsu]
-
-    # Grab closest values from the Hamamatsu dataset and plot them afterwards
-    plt.ion()
-    plt.figure(figsize=[16, 9])
-    CompareImages = []
-    for c, i in enumerate(ImageListERI):
-        print 80 * '-'
-        print 'Finding best matching current from Hamamatsu for %s (%s kV, ' \
-              '%s mA)' % (i, VoltageERI[c], CurrentERI[c])
-        Candidates = []
-        for k in ImageListHamamatsu:
-            if str(VoltageERI[c]) + 'kV' in k:
-                Candidates.append(k)
-        IndexList = [ImageListHamamatsu.index(i) for i in Candidates]
-        # Get the closest value to the current from Hamamatsu list. Based on
-        # http://stackoverflow.com/a/9706105/323100
-        ChosenOne = min(enumerate([CurrentHamamatsu[i] for i in IndexList]),
-                        key=lambda x: abs(x[1]-CurrentERI[c]))
-        print 'Found a match in %s' % Candidates[ChosenOne[0]]
-        CompareImages.append(Candidates[ChosenOne[0]])
-
-    print 80 * '-'
-
-    VoltageMatch = [int(os.path.basename(i).split('_')[1][:-2]) for i in
-                    CompareImages]
-    CurrentMatch = [int(os.path.basename(i).split('_')[2][:-2]) for i in
-                    CompareImages]
-
-    plt.scatter(VoltageHamamatsu, CurrentHamamatsu, c=colors[0], alpha=0.25,
-                label='Hamamatsu')
-    plt.plot(VoltageERI, CurrentERI, c=colors[1], label='ERI')
-    plt.plot(VoltageMatch, CurrentMatch, c=colors[2], label='Best Match')
-    plt.legend(loc='best')
-    # plt.tight_layout()
+# Display results
+plt.ion()
+plt.figure(figsize=[18, 13])
+gs = gridspec.GridSpec(4, 4)
+plt.subplot(gs[1:3, 0])
+plt.scatter(VoltageHamamatsu, CurrentHamamatsu, c=colors[0], alpha=0.25,
+            label='Hamamatsu')
+plt.plot(VoltageERI, CurrentERI, c=colors[1], label='ERI')
+plt.plot(VoltageMatch, CurrentMatch, c=colors[2], label='Closest Match')
+plt.xlabel('Voltage [kV]')
+plt.ylabel('Current [uA]')
+plt.ylim([0, 75])
+plt.legend(loc='best')
+plt.title('Voltage vs. Current')
+for c, i in enumerate(CompareImages):
+    print '%s/%s | %s kV/%s uA | Comparing %s with %s' % (
+        c + 1, len(CompareImages), VoltageERI[c], CurrentERI[c],
+        bold(os.path.basename(i)),
+        bold(os.path.basename(ImageListERI[c])))
+    ImageERI = plt.imread(ImageListERI[c])
+    ImageHamamatsu = plt.imread(i)
+    plt.subplot(gs[:2, 1:])
+    plt.imshow(plt.imread(ImageListERI[c]))
+    plt.axis('off')
+    plt.subplot(gs[2:, 1:])
+    plt.imshow(plt.imread(i))
+    plt.axis('off')
+    plt.subplots_adjust(wspace=0.001, hspace=0.15, left=0.02, right=1,
+                        top=0.98, bottom=0.01)
     plt.draw()
-    plt.savefig(os.path.join(os.path.expanduser('~'), 'Data20', 'CNT',
-                             'ERI-Analysis', 'Images',
-                             'Match-' +
-                             os.path.basename(os.path.dirname(
-                                 CompareImages[0])) + '_vs_' +
-                             os.path.basename(os.path.dirname(
-                                 ImageListERI[0])) + '.png'))
-
-    # Plot brightness of the comparable images in one plot
-    BrightnessHamamatsu = []
-    BrightnessERI = []
-    DisplayProgress = True
-    plt.figure(figsize=[16, 9])
-    for c, i in enumerate(CompareImages):
-        print '%s/%s: Comparing %s with %s' % (c + 1, len(CompareImages),
-                                               os.path.basename(i),
-                                               os.path.basename(
-                                                   ImageListERI[c]))
-        ImageERI = read_raw(ImageListERI[c])
-        ImageHamamatsu = read_raw(i)
-        BrightnessERI.append(numpy.mean(ImageERI))
-        BrightnessHamamatsu.append(numpy.mean(ImageHamamatsu))
-        # plt.subplot(221)
-        # plt.imshow(read_raw(ImageListERI[c]))
-        # plt.title('%s: %s' % (c, os.path.basename(ImageListERI[c])))
-        # plt.subplot(222)
-        # plt.imshow(read_raw(i))
-        # plt.title('%s: %s' % (c, os.path.basename(i)))
-        # plt.subplot(212)
-        # plt.cla()
-    # Plot Brightness with kV as x-axis (Image 0 is 25 kV)
-    plt.subplot(211)
-    plt.plot(range(25, 25 + len(BrightnessHamamatsu)), BrightnessHamamatsu,
-             c=colors[2], label=os.path.basename(os.path.dirname(i)))
-    plt.plot(range(25, 25 + len(BrightnessERI)), BrightnessERI, c=colors[1],
-             label=os.path.basename(os.path.dirname(ImageListERI[0])))
-    # ScalingHamamatsu = numpy.max(BrightnessHamamatsu) / \
-    #                    numpy.max(CurrentMatch)
-    # plt.plot(VoltageMatch, [i * ScalingHamamatsu for i in CurrentMatch],
-    #          c=colors[2], alpha=0.5, linestyle='--',
-    #          label='Hamamatsu, current scaled by %0.1f' % ScalingHamamatsu)
-    # ScalingERI = max(BrightnessERI) / float(max(CurrentERI))
-    # plt.plot(VoltageERI, [i * ScalingERI for i in CurrentERI], c=colors[1],
-    #          alpha=0.5, linestyle='--',
-    #          label='ERI: current scaled by %0.1f' % ScalingERI)
-    plt.xlim([20, 70])
-    plt.ylim([0, 2**12])
-    plt.xlabel('kV')
-    plt.ylabel('Mean image brightness')
-    plt.title('Average brightness compared to current')
-    plt.legend(loc='best')
-
-    plt.subplot(212)
-    plt.plot([a/b for a, b in zip(BrightnessHamamatsu, BrightnessERI)],
-             label='Hamamatsu / ERI')
-    plt.ylim([0, 6])
-    plt.legend(loc='best')
-    plt.title('Brightness difference')
-    # plt.tight_layout()
-    plt.draw()
-    plt.savefig(os.path.join(os.path.expanduser('~'), 'Data20', 'CNT',
-                             'ERI-Analysis', 'Images',
-                             'Brightness-' +
-                             os.path.basename(os.path.dirname(
-                                 CompareImages[0])) + '_vs_' +
-                             os.path.basename(os.path.dirname(
-                                 ImageListERI[0])) + '.png'))
-    plt.ioff()
-    plt.show()
-    print 'Done with case %s' % Case
+    plt.savefig(os.path.join(OutputPath, str(VoltageERI[c]) + 'kV' +
+                             str(CurrentERI[c]) + 'uA.png'),
+                bbox_inches='tight')
+plt.ioff()
+plt.show()
 print 'Done with everything!'
 
