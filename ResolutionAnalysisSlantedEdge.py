b2a2204a62f9f916a4464228877f0d08526f6caf
David Haberthür <email@davidhaberthuer.ch>
2016-01-29 17:05:46 +0100

    Now including MTF
    We're using the slanted edge of the resolution phantom to calculate the
    average edge response, from this the line spread function and from this
    the MTF.
    For the moment this is only calculated for two images.
    For the report, we have to add several MTFs to have a strong argument.

--- ResolutionAnalysisSlantedEdge.py
+++ ResolutionAnalysisSlantedEdge.py
@@ -9,13 +9,19 @@ Script to plot a line through some of the regions of the resolution phantom.
 # Imports
 import numpy
 import os
-import glob
+import platform
+import matplotlib
+# Make sure we are running a good version of matplotlib (i.e. > 1)
+if float(str(matplotlib.__version__)[:3]) < 1:
+    print '\nWe are running matplotlib version', matplotlib.__version__, 'on', \
+        platform.node()
+    print 'To make this script work, we need a matplotlib version > 1.'
+    print 'To load such a version, please enter the following command in', \
+        'the terminal and restart the script.'
+    print '\n\tmodule load xbl/epd_free/7.3-2-2013.06\n'
+    exit()
 import matplotlib.pylab as plt
 from matplotlib.patches import Rectangle
-import time
-
-import lineprofiler
-
 
 #  Functions
 def read_raw(filename, width=2048, height=1024, verbose=False):
@@ -34,7 +40,6 @@ def read_raw(filename, width=2048, height=1024, verbose=False):
     image = numpy.flipud(image)
     return image
 
-
 def contrast_stretch(image, std=3, verbose=False):
     """
     Clip image histogram to the mean \pm N standard deviations, according to
@@ -50,200 +55,123 @@ def contrast_stretch(image, std=3, verbose=False):
             clippedimage)) + ']'
     return clippedimage
 
+def gaussianfit(data):
+    from scipy.optimize import curve_fit
+    # Define gaussian model function which we use to calculate a fit to the data
+    def gauss(x, *p):
+        A, mu, sigma = p
+        return A * numpy.exp(-(x - mu)**2 / (2. * sigma**2))
+    # Fit with initial guess p0
+    p0 = [1, 0, 0.2]
+    coeff, var_matrix = curve_fit(gauss, range(0, len(data)), data, p0=p0)
+    return gauss(range(0,len(data)), *coeff)
+
+def LSF(edgespreadfunction):
+    return numpy.diff(edgespreadfunction)
+
+def MTF(linespreadfunction):
+    return numpy.abs(numpy.fft.fft(linespreadfunction))[:len(
+        linespreadfunction) / 2]
+
+def normalize(data):
+    return (data - numpy.min(data)) / (numpy.max(data) - numpy.min(data))
 
 # Display all images consistently
 plt.rc('image', cmap='gray', interpolation='nearest')
 # Make lines a bit wider
-plt.rc('lines', linewidth=2)
+plt.rc('lines', linewidth=3)
 # Show background grid
 plt.rc('axes', grid=True)
 # Colors from 'I want hue'
-UserColors = ["#84DEBD", "#D1B9D4", "#D1D171"]
+Colors = ["#E9AE70",
+"#7DDCD8",
+"#E7B0CF",
+"#9DE380",
+"#6CE3AF",
+"#ABC7E5",
+"#D8D867",
+"#B5D79B"]
 
-StartPath = '/afs/psi.ch/user/h/haberthuer/slsbl/x02da/e13960/Data20/Gantry/Images'
-Image1Name = os.path.join(StartPath, 'ERI-Grid4-15s-Exposure', 'ERI_060kV_029uA_15sExp_01.raw')
-Image2Name = os.path.join(StartPath, 'Hamamatsu-Grid4-15s-Exposure', 'Hamamatsu_060kV_030uA_15sExp_01.raw')
-
-Image1 = read_raw(Image1Name)
-Image2 = read_raw(Image2Name)
-
-Crop1 = Image1[100:900,1600:1750]
-Crop2 = Image2[100:900,1600:1750]
-
-plt.subplot(221)
-plt.imshow(Crop1)
-plt.title('ERI')
-plt.subplot(223)
-plt.imshow(Crop2)
-plt.title('Hamamatsu')
-
-plt.subplot(222)
-plt.plot(numpy.mean(Crop1, axis=0))
-plt.subplot(224)
-plt.plot(numpy.mean(Crop2, axis=0))
-plt.show()
-
-
-exit()
-
+# Load images
 StartPath = '/sls/X02DA/data/e13960/Data20/Gantry/Images'
-# Only do 'Grid' Folders
-FolderList = [i if 'Grid' in i else '' for i in sorted(os.walk(StartPath).next()[1])]
-# Disregard now emtpy list elements: http://stackoverflow.com/a/3845449
-FolderList = [x for x in FolderList if x]
-for Folder in FolderList:
-    FolderToLookAt = os.path.join(StartPath, Folder)
-    ImageList = sorted(glob.glob(os.path.join(FolderToLookAt, '*.raw')))
+ERIName = os.path.join(StartPath, 'ERI-Grid4-15s-Exposure',
+                       'ERI_060kV_029uA_15sExp_01.raw')
+HamamatsuName = os.path.join(StartPath, 'Hamamatsu-Grid4-15s-Exposure',
+                             'Hamamatsu_060kV_030uA_15sExp_01.raw')
+ERIImage = read_raw(ERIName)
+HamamatsuImage = read_raw(HamamatsuName)
 
-    # Prepare output directory
-    try:
-        os.makedirs(os.path.join(os.path.expanduser('~'), 'Data20', 'CNT',
-                                 'ERI-Analysis', 'Images',
-                                 'Resolution-' +
-                                 os.path.basename(FolderToLookAt)))
-    except OSError:
-        # Directory already exists
-        pass
+# Crop to interesting region (slanted edge of resolution phantom)
+CropRegion = [100, 900, 550, 700]  # left
+CropRegion = [100, 900, 1600, 1750]  # right
+ERICrop = ERIImage[CropRegion[0]:CropRegion[1], CropRegion[2]:CropRegion[3]]
+HamamatsuCrop = HamamatsuImage[CropRegion[0]:CropRegion[1],
+                               CropRegion[2]:CropRegion[3]]
 
-    # Prepare figure
-    plt.ion()
-    plt.figure(figsize=[20, 9])
-    plt.suptitle(os.path.basename(FolderToLookAt))
+# Calculate average Edge response
+ERIResponse = numpy.mean(ERICrop, axis=0)
+HamamatsuResponse = numpy.mean(HamamatsuCrop, axis=0)
 
-    # Grab voltage and current values from all images in folder to plot them
-    # later on
-    print 'Reading voltage and current from %s images in folder %s' % (
-        len(ImageList), FolderToLookAt)
-    Voltage = [int(os.path.basename(image).split('_')[1][:-2]) for image in
-               ImageList]
-    Current = [int(os.path.basename(image).split('_')[2][:-2]) for image in
-               ImageList]
+# Display images with region of crop
+plt.figure(figsize=[20, 9])
+plt.subplot(251)
+plt.title(os.path.basename(ERIName))
+plt.imshow(ERIImage)
+# Draw crop region in original image
+currentAxis = plt.gca()
+currentAxis.add_patch(Rectangle((CropRegion[2], CropRegion[0]),
+                                CropRegion[3] - CropRegion[2],
+                                CropRegion[1] - CropRegion[0], facecolor='red',
+                                edgecolor='w', alpha=0.125))
+plt.subplot(256)
+plt.title(os.path.basename(HamamatsuName))
+plt.imshow(HamamatsuImage)
+currentAxis = plt.gca()
+currentAxis.add_patch(Rectangle((CropRegion[2], CropRegion[0]),
+                                CropRegion[3] - CropRegion[2],
+                                CropRegion[1] - CropRegion[0], facecolor='red',
+                                edgecolor='w', alpha=0.125))
+# Display cropped region
+plt.subplot(252)
+plt.title('ERI')
+plt.imshow(ERICrop)
+plt.subplot(257)
+plt.title('Hamamatsu')
+plt.imshow(HamamatsuCrop)
+plt.subplot(153)
+plt.title('Averaged edge response')
+plt.plot(ERIResponse, c=Colors[0], label='ERI')
+plt.plot(HamamatsuResponse, c=Colors[1], label='Hamamatsu')
+# Plot some (non-averaged) edge responses, too
+for ctr, line in enumerate(range(100, 800, 200)):
+    plt.subplot(252)
+    plt.axhline(line, c=Colors[ctr+2])
+    plt.subplot(257)
+    plt.axhline(line, c=Colors[ctr+2])
+    plt.subplot(153)
+    plt.plot(ERICrop[line, :], alpha=0.309, c=Colors[ctr+2])
+    plt.plot(HamamatsuCrop[line, :], alpha=0.309, c=Colors[ctr+2])
+plt.legend(loc='lower right')
 
-    # Select three lines through resolution phantom
-    LineYStart = 100
-    LineLength = 850
-    Coordinates = [((850, LineYStart), (850, LineYStart + LineLength)),
-                   ((1150, LineYStart), (1150, LineYStart + LineLength)),
-                   ((1500, LineYStart), (1500, LineYStart + LineLength))]
-    # Plot everything
-    for ImageCounter, ImageName in enumerate(ImageList):
-        print '%02d/%s: Reading image %s and plotting line profiles' % (
-            ImageCounter + 1, len(ImageList), os.path.basename(ImageName))
-        ThisVoltage = int(os.path.basename(ImageName).split('_')[1][:-2])
-        ThisCurrent = int(os.path.basename(ImageName).split('_')[2][:-2])
-        # Only do the thing if the current is not larger than the exponential
-        # fit that we found with VoltageVsCurrentExponentialFit.py, which is
-        # 1.05e-01 * numpy.exp(9.04e-02 * x) + 8.72e+00 (and a safety margin).
-        # Or the voltage is not larger than 66 keV
-        if ThisCurrent < 1.05e-1 * numpy.exp(9.04e-2 * ThisVoltage) + 20 and \
-                ThisVoltage < 66:
-            # Which image are we looking at from all the ones recorded?
-            plt.subplot(421)
-            plt.cla()
-            plt.title('Voltage vs. current')
-            plt.scatter(Voltage, Current)
-            plt.plot(ThisVoltage, ThisCurrent, color='red', marker='o',
-                     markersize=15, alpha=0.309)
-            plt.xlabel('Voltage [kV]')
-            plt.ylabel('Current [uA]')
-            plt.xlim([20, 70])
-            plt.ylim([0, 75])
-            # Show the current image
-            plt.subplot(422)
-            plt.cla()
-            plt.title('Contrast stretched Image %s/%s: %s\nWith line profiles '
-                      'and red region used for mean and STD' % (
-                          ImageCounter + 1, len(ImageList),
-                          os.path.basename(ImageName)))
-            Img = read_raw(ImageName)
-            plt.imshow(contrast_stretch(Img))
-            # Show where line profiles have been calculated
-            for CoordinateCounter, CurrentCoordinates in enumerate(Coordinates):
-                SelectedPoints, LineProfile = lineprofiler.lineprofile(
-                    Img, CurrentCoordinates, showimage=False)
-                plt.plot((SelectedPoints[0][0], SelectedPoints[1][0]),
-                         (SelectedPoints[0][1], SelectedPoints[1][1]),
-                         color=UserColors[CoordinateCounter], marker='o')
-                plt.plot(SelectedPoints[0][0], SelectedPoints[0][1],
-                         color='yellow', marker='o', alpha=0.618)
-                plt.plot(SelectedPoints[1][0], SelectedPoints[1][1],
-                         color='black', marker='o', alpha=0.618)
-            plt.axis('off')
-            # Crop region for Standard deviation
-            CropSize = 900
-            CropStart = [50, 700]
-            Crop = Img[CropStart[0]:CropStart[0] + CropSize,
-                       CropStart[1]:CropStart[1] + CropSize]
-            # Draw crop region in original image
-            currentAxis = plt.gca()
-            currentAxis.add_patch(Rectangle((CropStart[1], CropStart[0]),
-                                            CropSize, CropSize, facecolor='red',
-                                            edgecolor='w', alpha=0.125))
-            # Show the line profiles
-            for CoordinateCounter, CurrentCoordinates in enumerate(Coordinates):
-                plt.subplot(5, 1, 3 + CoordinateCounter)
-                plt.cla()
-                if not CoordinateCounter:
-                    # Title the first of the three line plots
-                    plt.title('Red = mean (%0.2f) +- STD of cropped region, '
-                              'grey = mean +- 2 x STD.)' % numpy.mean(Crop))
-                if 'Grid' in os.path.basename(FolderToLookAt):
-                    xStart = 240
-                    xShift = 320
-                    yStart = numpy.mean(Crop) + 150
-                    if CoordinateCounter == 2:
-                        # Annotate first line profile if we have imaged the grid
-                        plt.annotate('2.8 lp/mm', xy=(xStart + 1 * xShift, yStart))
-                        plt.annotate('2.5 lp/mm', xy=(xStart + 2 * xShift, yStart))
-                        plt.annotate('2.2 lp/mm', xy=(xStart + 3 * xShift, yStart))
-                        plt.annotate('2.0 lp/mm', xy=(xStart + 4 * xShift, yStart))
-                    elif CoordinateCounter == 1:
-                        # Annotate second line profile
-                        plt.annotate('4.3 lp/mm', xy=(xStart, yStart))
-                        plt.annotate('4.0 lp/mm', xy=(xStart + 1 * xShift, yStart))
-                        plt.annotate('3.7 lp/mm', xy=(xStart + 2 * xShift, yStart))
-                        plt.annotate('3.4 lp/mm', xy=(xStart + 3 * xShift, yStart))
-                        plt.annotate('3.1 lp/mm', xy=(xStart + 4 * xShift, yStart))
-                    else:
-                        # Annotate third line profile
-                        plt.annotate('6.0 lp/mm', xy=(xStart, yStart))
-                        plt.annotate('5.6 lp/mm', xy=(xStart + 1 * xShift, yStart))
-                        plt.annotate('5.3 lp/mm', xy=(xStart + 2 * xShift, yStart))
-                        plt.annotate('5.0 lp/mm', xy=(xStart + 3 * xShift, yStart))
-                        plt.annotate('4.6 lp/mm', xy=(xStart + 4 * xShift, yStart))
-                # Gray region to 2xSTD
-                plt.fill_between(range(2048),
-                                 numpy.mean(Crop) + 2 * numpy.std(Crop),
-                                 numpy.mean(Crop) - 2 * numpy.std(Crop),
-                                 color='k', alpha=0.5, linewidth=1)
-                # Red region to 2xSTD
-                plt.fill_between(range(2048),
-                                 numpy.mean(Crop) + numpy.std(Crop),
-                                 numpy.mean(Crop) - numpy.std(Crop),
-                                 color='r', linewidth=1)
-                # Plot mean and STD of cropped region
-                plt.axhline(numpy.mean(Crop), linestyle='-',
-                            color='k', linewidth=1)
-                SelectedPoints, LineProfile = lineprofiler.lineprofile(
-                    Img, CurrentCoordinates, showimage=False)
-                plt.plot(LineProfile, color=UserColors[CoordinateCounter])
-                plt.xlim([0, len(LineProfile)])
-                plt.ylim([0, 2 ** 12])
-                plt.plot(0, numpy.mean(Crop), color='yellow', marker='o',
-                         markersize=15, alpha=0.618)
-                plt.plot(len(LineProfile) - 1, numpy.mean(Crop), color='black',
-                         marker='o', markersize=15, alpha=0.618)
-                plt.xlim([0, len(LineProfile)])
-                plt.ylim([0, 2 ** 12])
-            plt.draw()
-            plt.savefig(os.path.join(os.path.expanduser('~'), 'Data20', 'CNT',
-                                     'ERI-Analysis', 'Images',
-                                     'Resolution-' + os.path.basename(FolderToLookAt),
-                                     os.path.splitext(os.path.basename(ImageName))[0] + '.png'))
-        else:
-            print '\tThe current of %s uA is larger than the fit of %0.2f uA, ' \
-                  'or the voltage is later than 65 keV thus not looking at ' \
-                  'the image' % (ThisCurrent,
-                                 ThisVoltage * 0.1 * numpy.exp(9.04e-2) + 10)
-    plt.ioff()
-    plt.close('all')
+# Calculate MTF.
+# According to http://www.imatest.com/docs/validating_slanted_edge/, the MTF is
+# the Fourier transform of the line spread function, which is the derivative of
+# the averaged edge.
+# We thus calculate the average edge ($source$Response) and take the derivative
+# of this (with the `LSF` function, which uses `numpy.diff`).
+# To the derivative we fit a gaussian function with the `gaussianfit` function
+# which then acts as an input for the `MTF` function above.
+plt.subplot(154)
+plt.title('Line spread function')
+plt.plot(LSF(ERIResponse), c=Colors[2], alpha=0.5, label='ERI Data')
+plt.plot(LSF(HamamatsuResponse), c=Colors[3], alpha=0.5, label='Hamamatsu Data')
+plt.plot(gaussianfit(LSF(ERIResponse)), c=Colors[0], label='ERI fit')
+plt.plot(gaussianfit(LSF(HamamatsuResponse)), c=Colors[1], label='Hamamatsu fit')
+plt.legend(loc='best')
+plt.subplot(155)
+plt.plot(MTF(gaussianfit(LSF(ERIResponse))), c=Colors[0],
+         label='MTF ERI')
+plt.plot(MTF(gaussianfit(LSF(HamamatsuResponse))), c=Colors[1],
+         label='MTF Hamamatsu')
+plt.legend(loc='best')
+plt.show()
